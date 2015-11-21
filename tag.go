c63ab5ff3af145df81b00231fed34a981d57b3aa
Aleks Kamko <aykamko@gmail.com>
2015-11-21 12:56:39 -0800

    speedup by avoiding string conversion

--- tag.go
+++ tag.go
@@ -20,11 +20,9 @@ func check(e error) {
 }
 
 var (
-	red        = color.RedString
-	blue       = color.BlueString
-	ansi       = regexp.MustCompile(`\x1B\[([0-9]{1,2}(;[0-9]{1,2})*)?[a-zA-Z]`)
-	lineNumber = regexp.MustCompile(`^(\d+):`)
-	cwd        string
+	red  = color.RedString
+	blue = color.BlueString
+	cwd  string
 )
 
 type AliasFile struct {
@@ -50,47 +48,81 @@ func (a *AliasFile) WriteFile(filename string) {
 	check(err)
 }
 
-func main() {
-	cmd := exec.Command("ag", append([]string{"--group", "--color"}, os.Args[1:]...)...)
+func getOutputColors(args []string) (pathColor string, lineNumberColor string) {
+	pathColor, lineNumberColor = "1;32", "1;33"
+	for i, flag := range args {
+		if flag == "--color-path" && i+1 < len(args) {
+			pathColor = args[i+1]
+		} else if flag == "--color-line-number" && i+1 < len(args) {
+			lineNumberColor = args[i+1]
+		}
+	}
+	return
+}
+
+func generateShortcuts(cmd *exec.Cmd) {
+	pathColor, lineNumberColor := getOutputColors(os.Args[1:])
+	pathRe := regexp.MustCompile(
+		fmt.Sprintf(`^\x1b\[%sm([^\x1b]+)`, pathColor))
+	lineNumberRe := regexp.MustCompile(
+		fmt.Sprintf(`^\x1b\[%sm(\d+)\x1b\[0m\x1b\[K:`, lineNumberColor))
+
 	stdout, err := cmd.StdoutPipe()
 	check(err)
-
 	scanner := bufio.NewScanner(stdout)
-	err = cmd.Start()
-	check(err)
 
 	cwd, err = os.Getwd()
 	check(err)
 
 	var (
-		line         string
-		strippedLine string
-		curfile      string
-		groups       []string
+		line      []byte
+		curPath   string
+		groupIdxs []int
 	)
 	aliasFile := NewAliasFile()
 	aliasIndex := 1
 
+	err = cmd.Start()
+	check(err)
+
 	for scanner.Scan() {
-		line = scanner.Text()
-		strippedLine = stripAnsi(line)
+		line = scanner.Bytes()
 		if len(line) > 0 {
-			groups = lineNumber.FindStringSubmatch(strippedLine)
-			if len(groups) > 0 {
-				aliasFile.WriteAlias(aliasIndex, curfile, stripAnsi(groups[1]))
-				fmt.Printf("%s%s%s %s\n", blue("["), red(fmt.Sprintf("%d", aliasIndex)), blue("]"), line)
+			if groupIdxs = lineNumberRe.FindSubmatchIndex(line); len(groupIdxs) > 0 {
+				aliasFile.WriteAlias(aliasIndex, curPath, string(line[groupIdxs[2]:groupIdxs[3]]))
+				fmt.Printf("%s%s%s %s\n", blue("["), red("%d", aliasIndex), blue("]"), string(line))
 				aliasIndex++
 				continue
+			} else if groupIdxs = pathRe.FindSubmatchIndex(line); len(groupIdxs) > 0 {
+				curPath = string(line[groupIdxs[2]:groupIdxs[3]])
 			}
-
-			curfile = strippedLine
 		}
-		fmt.Println(line)
+		fmt.Println(string(line))
 	}
 
 	aliasFile.WriteFile("/tmp/tag_aliases")
 }
 
-func stripAnsi(str string) string {
-	return ansi.ReplaceAllString(str, "")
+func passThrough(cmd *exec.Cmd) {
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	if err := cmd.Run(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func main() {
+	args := []string{"--group"}
+	args = append(args, os.Args[1:]...)
+	args = append(args, "--color")
+
+	cmd := exec.Command("ag", args...)
+
+	stat, _ := os.Stdin.Stat()
+	if (stat.Mode() & os.ModeCharDevice) == 0 { // Data being piped from stdin
+		passThrough(cmd)
+		return
+	}
+
+	generateShortcuts(cmd)
 }
