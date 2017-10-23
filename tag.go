ee5f1eef078e44252f8cf7c6518722652e9b931e
Aleks Kamko <aykamko@gmail.com>
2017-10-22 17:25:42 -0700

    hotfix: aliasCmdFmtString error

--- tag.go
+++ tag.go
@@ -77,7 +77,7 @@ func NewAliasFile() *AliasFile {
 	aliasPrefix := getEnvDefault("TAG_ALIAS_PREFIX", "e")
 	aliasCmdFmtString := getEnvDefault(
 		"TAG_CMD_FMT_STRING",
-		"vim -c 'call cursor({{.LineNumber}}, {{.ColumnNumber}})' '{{.Filename}}'")
+		`vim -c "call cursor({{.LineNumber}}, {{.ColumnNumber}})" "{{.Filename}}"`)
 
 	a := &AliasFile{
 		fmtStr:   "alias " + aliasPrefix + "{{.MatchIndex}}='" + aliasCmdFmtString + "'\n",
